429cb417dd1854851a5320713999a8b76ac3388f
vikaskheni <vikaskheni30@gmail.com>
2024-06-02 13:55:20 +0530

    Problems added

---
+++ problems/Array Algotithm/Reverse_pairs.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int count = 0;
+    void merge(vector<int> &arr, int low, int mid, int high)
+    {
+        vector<int> temp;    // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1; // starting index of right half of arr
+
+        while (left <= mid && right <= high)
+        {
+            if (arr[left] <= arr[right])
+            {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else
+            {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid)
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high)
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++)
+        {
+            arr[i] = temp[i - low];
+        }
+    }
+    void count23(vector<int> &arr, int low, int mid, int high)
+    {
+        int right = mid + 1;
+        for (int i = low; i <= mid; i++)
+        {
+            while (right <= high && arr[i] > (long long)2 * arr[right])
+            {
+                right++;
+            }
+            count += (right - (mid + 1));
+        }
+    }
+    void mergeSort(vector<int> &arr, int low, int high)
+    {
+        if (low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);      // left half
+        mergeSort(arr, mid + 1, high); // right half
+        count23(arr, low, mid, high);
+        merge(arr, low, mid, high); // merging sorted halves
+    }
+
+    int reversePairs(vector<int> &nums)
+    {
+        mergeSort(nums, 0, nums.size() - 1);
+        return count;
+    }
+};
